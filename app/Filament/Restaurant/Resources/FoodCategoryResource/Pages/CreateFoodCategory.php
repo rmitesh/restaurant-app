a981f326e3d375da971367e805324a718da2038f
Mitesh Rathod <48554454+rmitesh@users.noreply.github.com>
2023-10-21 19:16:18 +0530

    fix: added slug while create & update

--- app/Filament/Restaurant/Resources/FoodCategoryResource/Pages/CreateFoodCategory.php
+++ app/Filament/Restaurant/Resources/FoodCategoryResource/Pages/CreateFoodCategory.php
@@ -5,8 +5,16 @@ namespace App\Filament\Restaurant\Resources\FoodCategoryResource\Pages;
 use App\Filament\Restaurant\Resources\FoodCategoryResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class CreateFoodCategory extends CreateRecord
 {
     protected static string $resource = FoodCategoryResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $data['slug'] = Str::slug($data['name']);
+        return static::getModel()::create($data);
+    }
 }
--- app/Filament/Restaurant/Resources/FoodCategoryResource/Pages/EditFoodCategory.php
+++ app/Filament/Restaurant/Resources/FoodCategoryResource/Pages/EditFoodCategory.php
@@ -5,10 +5,21 @@ namespace App\Filament\Restaurant\Resources\FoodCategoryResource\Pages;
 use App\Filament\Restaurant\Resources\FoodCategoryResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class EditFoodCategory extends EditRecord
 {
     protected static string $resource = FoodCategoryResource::class;
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $data['slug'] = Str::slug($data['name']);
+     
+        $record->update($data);
+ 
+        return $record;
+    }
 
     protected function getHeaderActions(): array
     {
